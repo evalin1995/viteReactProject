277a1e87dbe83191885fa4742d118ba42924eaee
evalin1995 <nhglje250@gmail.com>
2024-11-20 15:51:18 +0800

    1120

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV ==='production' ?'/demoProject/':'/',
+  base:process.env.NODE_ENV ==='production' ?'/viteReactProject/':'/',
   plugins: [react()],
 })
